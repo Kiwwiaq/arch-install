46e4772e190b21718c39b51e3be2e4285d64a833
Kiwwiaq <ivan.waginger@gmail.com>
2020-04-27 22:45:57 +0200

    - enable lightdm service - regen locale

--- 03-base-gui-vbox.sh
+++ 03-base-gui-vbox.sh
@@ -3,7 +3,14 @@ clear
 
 # Install basic GUI
 echo "Installing ibasic GUI..."
-sudo pacman --noconfirm --needed -S virtualbox-guest-utils xorg-server lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings cinnamon
+pacman --noconfirm --needed -S virtualbox-guest-utils xorg-server lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings cinnamon
 
 # Install base software
-sudo pacman --noconfirm --needed -S gnome-terminal
+pacman --noconfirm --needed -S gnome-terminal
+
+# Enable GUI
+systemctl enable lightdm.service
+
+# Gnome terminal fix
+sudo locale-gen
+sudo localectl set-locale LANG="en_US.UTF-8"
